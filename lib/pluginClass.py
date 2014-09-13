5b5add3698d077a9cb2b4439fec5b216bac50153
Gianluca Sabella <gianluca.sabella@gmail.com>
2014-09-14 00:41:44 +0200

    updated general pluginClass

--- lib/pluginClass.py
+++ lib/pluginClass.py
@@ -21,14 +21,48 @@ import libQueue
 class gmpPlugin(object):
     def __init__(self):
         self.type='generic'
+        self.plan=list()
         pass
     
-    def downloadPlan(self):
+    def getPlan(self):
         #Return a generic Plan as dictionary
         sample=dict()
         return sample
     
-    def storePlan(self,plan):
+    def storePlan(self):
         #store the plan into the DB
         print "Storing the plan into the DB"
+        x=libQueue.queue()
+        i=0
+        for plannedItem in self.plan:
+            i+=1
+            if i>10:
+                return
+            try:
+                x.addItem(plannedItem)
+            except:
+                print "Failed to import product %s" % plannedItem.ID
         return
+    
+    def getAllMetalinks(self):
+        #empty funtion; do nothing
+        return
+    
+    def getMetalink(self,queuedItem):
+        #empty funtion; do nothing
+        return
+    
+    
+def getPlugin(target):
+    if target=='dhus':
+        import pluginDhus
+        x=pluginDhus.gmpPluginDhus()
+        return x
+    if target=='oda':
+        import pluginOda
+        x=pluginOda.gmpPluginOda()
+        return x
+    return
+    
+    
+    
