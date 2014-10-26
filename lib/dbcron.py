328e5c930e2c9d43b0f7c410ce2c331c5d8e2cb5
Gianluca Sabella <gianluca.sabella@gmail.com>
2014-10-26 20:17:58 +0100

    Added DB Cron script

---
+++ lib/dbcron.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#
+###########################################################
+#                                                         #
+# Project: GMP                                            #
+# Author:  gianluca.sabella@gmail.com                     #
+#                                                         #
+# Module:  config.py                                      #
+# First version: 18/10/2014                               #
+#                                                         #
+###########################################################
+
+import os,sys
+thisFolder=os.path.dirname(__file__)
+prjFolder=os.path.split(thisFolder)[0]
+sys.path.append(prjFolder+'/lib')
+
+import dbif
+
+sql=list()
+sql.append(
+  "update queue set dwnstatus='Q' where status='CATALOGUED' and pid is Null and dwnstatus='N';"
+)
+
+def main():
+    db=dbif.gencur('SELECT * FROM queue')
+    for isql in sql:
+        out=isql[:50]
+        try:
+            db.exe(isql)
+            out+= ' OK'
+        except:
+            out+= ' ERROR'
+        print out
+    pass
+
+if __name__ == "__main__":
+    main()
