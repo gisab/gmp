08a830136552520293938c0330ebef9ae5d7ceb1
Gianluca Sabella <gianluca.sabella@gmail.com>
2014-12-11 22:02:54 +0100

    Update dbcron for SLC pairs discovery

--- lib/dbcron.py
+++ lib/dbcron.py
@@ -29,7 +29,7 @@ sql.append(
 sql.append("""
     INSERT INTO slc (name, area,producttype,relativeorbit)
     SELECT
-       concat('Area',floor(Area(ST_Intersection(p1.footprint, p2.footprint))/Area(p1.footprint)*100)),
+       concat('Area_',floor(Area(ST_Intersection(p1.footprint, p2.footprint))/Area(p1.footprint)*100)),
        ST_Intersection(p1.footprint, p2.footprint),
        p1.producttype,
        mod(p1.orbit -73, 175)+1 relorb
@@ -44,21 +44,64 @@ sql.append("""
        mod(p1.orbit,175)=mod(p2.orbit,175) and
        AsText(ST_Intersection(p1.footprint, p2.footprint)) is not null and
        Area(ST_Intersection(p1.footprint, p2.footprint))/Area(p1.footprint)>0.6
-    ORDER BY Area(ST_Intersection(p1.footprint, p2.footprint))/Area(p1.footprint) desc limit 1;
+    ORDER BY Area(ST_Intersection(p1.footprint, p2.footprint))/Area(p1.footprint) desc limit 10;
 """)
-sql.append("""
+'''sql.append("""
     UPDATE product P INNER JOIN slc S on P.producttype=S.producttype
     SET P.SLCID=S.ID
     WHERE
      P.SLCID is null and
      mod(P.orbit -73, 175)+1 = S.relativeorbit and
-     Area(ST_Intersection(P.footprint, S.area))/Area(P.footprint)>0.6;
+     Area(ST_Intersection(P.footprint, S.area))/Area(P.footprint)>0.7;
 """)
+'''
+
+sql.append("truncate table product_slc;")
+sql.append("""insert into product_slc (productid, slcid, area_intersection, area_product)
+SELECT P.id, S.id,
+    st_area(st_intersection(P.footprint, S.area)),
+    st_area(P.footprint)
+FROM product P JOIN slc S ON P.producttype=S.producttype
+WHERE
+    isnull(P.slcid) and (((P.orbit - 73) % 175) + 1)=S.relativeorbit and
+    st_area(st_intersection(P.footprint, S.area))IS NOT NULL;
+""")
+sql.append("""UPDATE product P INNER JOIN product_slc PS on P.id=PS.productid
+SET P.SLCID=PS.slcid
+WHERE ratio>0.7;
+""")
+
+sql.append("""UPDATE slc S INNER JOIN area A
+SET S.name =A.name
+WHERE substr(S.name,1,4)='Area' and
+Area(ST_Intersection(S.area, A.geom))/Area(S.area)>0.9;
+""")
+sql.append("""UPDATE slc S INNER JOIN area A
+SET S.name =A.name
+WHERE substr(S.name,1,4)='Area' and
+Area(ST_Intersection(S.area, A.geom))/Area(S.area)>0.8;
+""")
+sql.append("""UPDATE slc S INNER JOIN area A
+SET S.name =A.name
+WHERE substr(S.name,1,4)='Area' and
+Area(ST_Intersection(S.area, A.geom))/Area(S.area)>0.7;
+""")
+sql.append("""UPDATE slc S INNER JOIN area A
+SET S.name =A.name
+WHERE substr(S.name,1,4)='Area' and
+Area(ST_Intersection(S.area, A.geom))/Area(S.area)>0.6;
+""")
+sql.append("""UPDATE slc S INNER JOIN area A
+SET S.name =A.name
+WHERE substr(S.name,1,4)='Area' and
+Area(ST_Intersection(S.area, A.geom))/Area(S.area)>0.2;
+""")
+
 
 def main():
     db=dbif.gencur('SELECT * FROM queue')
     for isql in sql:
-        out=isql[:50]
+        out=isql.strip().replace('\n',' ')[:50]
         try:
             db.exe(isql)
             out+= ' OK'
